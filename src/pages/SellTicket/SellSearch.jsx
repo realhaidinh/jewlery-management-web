bf7d5d8aea8d1237ef97e4253830332fa7d4e5b6
HungTuan-Onzo <hungtuan.onzo@gmail.com>
2023-05-09 03:12:00 +0700

    add ProductDetailModal

--- src/pages/SellTicket/SellSearch.jsx
+++ src/pages/SellTicket/SellSearch.jsx
@@ -1,71 +1,85 @@
-import { useState } from 'react';
-import { Button } from '@mui/material';
-import { ControlButton } from '../../components/Controls';
+import { useState, useMemo } from 'react';
 import { SearchContainer, TableContainer } from '../../components/Container/';
-import InfoIcon from '@mui/icons-material/Info';
 import formData from '../formData';
+import ProductDetailModal from '../../components/Modal/ProductDetailModal';
 
 const initialSearchInput = '';
-const columns = [
-  {
-    field: 'numOrder',
-    headerClassName: 'table-header',
-    headerName: '#',
-    headerAlign: 'center',
-    align: 'center',
-    width: 50,
-  },
-  { field: 'formID', headerClassName: 'table-header', headerName: 'Mã phiếu', width: 100 },
-  { field: 'customerName', headerClassName: 'table-header', headerName: 'Khách hàng', width: 350 },
-  {
-    field: 'totalPaid',
-    headerClassName: 'table-header',
-    headerName: 'Tổng thanh toán',
-    headerAlign: 'center',
-    align: 'center',
-    width: 170,
-  },
-  {
-    field: 'formDate',
-    headerClassName: 'table-header',
-    headerName: 'Ngày thanh toán',
-    headerAlign: 'center',
-    align: 'center',
-    width: 170,
-  },
-  {
-    field: 'actions',
-    type: 'actions',
-    headerClassName: 'table-header',
-    align: 'center',
-    width: 100,
-    getActions: (param) => [
-      <ControlButton variant="text" color="secondary" endIcon={<InfoIcon />}>
-        Chi tiết
-      </ControlButton>,
-    ],
-  },
-];
-const rows = formData.map((form, index) => {
-  return {
-    id: index,
-    numOrder: index + 1,
-    formID: form.formID,
-    customerName: form.customerName,
-    totalPaid: form.totalPaid,
-    formDate: form.dateCreated,
-  };
-});
 
 const SellSearch = ({ show }) => {
-  const [SearchInput, setSearchInput] = useState(initialSearchInput);
-  // console.log(SearchInput);
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason !== 'backdropClick') {
+      setOpen(false);
+    }
+  };
+
   const handleSearchInput = (event) => {
     setSearchInput(event.target.value);
   };
+
   const deleteSearchInput = () => {
     setSearchInput(initialSearchInput);
   };
+
+  const [SearchInput, setSearchInput] = useState(initialSearchInput);
+
+  const columns = [
+    {
+      field: 'numOrder',
+      headerClassName: 'table-header',
+      headerName: '#',
+      headerAlign: 'center',
+      align: 'center',
+      width: 50,
+    },
+    { field: 'formID', headerClassName: 'table-header', headerName: 'Mã phiếu', width: 100 },
+    { field: 'customerName', headerClassName: 'table-header', headerName: 'Khách hàng', width: 350 },
+    {
+      field: 'totalPaid',
+      headerClassName: 'table-header',
+      headerName: 'Tổng thanh toán',
+      headerAlign: 'center',
+      align: 'center',
+      width: 170,
+    },
+    {
+      field: 'formDate',
+      headerClassName: 'table-header',
+      headerName: 'Ngày thanh toán',
+      headerAlign: 'center',
+      align: 'center',
+      width: 170,
+    },
+    {
+      field: 'actions',
+      type: 'actions',
+      headerClassName: 'table-header',
+      align: 'center',
+      width: 100,
+      getActions: (param) => [
+        <ProductDetailModal open={open} onButtonClick={handleClickOpen} onButtonClose={handleClose} />,
+      ],
+    },
+  ];
+
+  const rows = useMemo(() => {
+    return formData.map((form, index) => {
+      return {
+        id: index,
+        numOrder: index + 1,
+        formID: form.formID,
+        customerName: form.customerName,
+        totalPaid: form.totalPaid,
+        formDate: form.dateCreated,
+      };
+    });
+  }, [formData]);
+
   return (
     <SearchContainer
       show={show}
